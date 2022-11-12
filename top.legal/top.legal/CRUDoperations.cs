aedefc57261d4b5b36f651b43665d9987971e23d
deepusiby100 <35959591+deepusiby100@users.noreply.github.com>
2022-11-12 08:32:58 +0200

    CRUDoperations added

---
+++ top.legal/top.legal/CRUDoperations.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+
+namespace top.legal
+{
+    class CRUDoperations
+    {
+        public void PerformCRUD(IOrganizationService svc)
+        {
+            //CREATE
+            var toplegalAccount = new Entity("account");
+            toplegalAccount.Attributes["name"] = "Top.Legal";
+            toplegalAccount.Attributes["websiteurl"] = "top.legal";
+            Guid accountID = svc.Create(toplegalAccount);
+            Console.WriteLine("Account created with ID - " + accountID);
+
+            //RETRIEVE  
+            Entity retrievedAccount = svc.Retrieve("account", accountID, new ColumnSet("name", "websiteurl"));
+            Console.WriteLine("Account name retrieved is - " + retrievedAccount.Attributes["name"]);
+
+            //UPDATE
+            Entity myAccount = new Entity("account");
+            myAccount.Id = accountID;
+            myAccount.Attributes["name"] = "Top.Legal Consultants";
+            svc.Update(myAccount);
+            Console.WriteLine("Account name updated as " + myAccount.Attributes["name"]);
+
+
+            //DELETE
+            svc.Delete("account", accountID);
+            Console.WriteLine("Account-" + myAccount.Attributes["name"] + " deleted");
+        }
+    }
+}
