0bb73d58962c559a3afb5342e25e6341e7291172
deepusiby100 <35959591+deepusiby100@users.noreply.github.com>
2022-11-12 08:36:31 +0200

    Connection Parameters loaded

--- top.legal/top.legal/Program.cs
+++ top.legal/top.legal/Program.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
 
 namespace top.legal
 {
-    class Program
+    class Program : OAuthConn
     {
         static void Main(string[] args)
         {
-
-
+            CRUDoperations CRUD = new CRUDoperations();
+            IOrganizationService svc = AADappConnection("2f388ee8-6fa8-46bf-906d-e603ace90d84",
+                                                        "3KV8Q~G1VfcdvmdfXkOk-v32ePQ2cCqhnwIwVde8",
+                                                        "https://toplegal.crm4.dynamics.com/");
+            var response = svc.Execute(new WhoAmIRequest());
+            CRUD.PerformCRUD(svc);
         }
     }
 }
